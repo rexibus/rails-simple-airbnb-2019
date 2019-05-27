a146e74f131dc0d6e4dd15d08dfa125f51870bb7
RegisWeill <regis_weill@hotmail.com>
2019-05-28 01:19:28 +0200

    controller new create show + views no style

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,30 @@
 class FlatsController < ApplicationController
+
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(params_flat)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  private
+
+  def params_flat
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
 end
